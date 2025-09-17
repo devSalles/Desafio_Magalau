58c820aabc35e95751d4de232bc4a71a7e00f776
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-09-17 15:35:41 -0300

    Realizando upload do arquivo

---
+++ Desafio_Magalu/src/test/java/com/example/Desafio_Magalu/DesafioMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Desafio_Magalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
