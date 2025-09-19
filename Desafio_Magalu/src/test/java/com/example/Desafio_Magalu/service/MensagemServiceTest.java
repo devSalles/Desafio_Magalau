be14e3aced18a3d42f6da36e54115a73b175b2a1
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-09-19 15:56:15 -0300

    Adicionado a camada de teste e a dependendcia do H2

---
+++ Desafio_Magalu/src/test/java/com/example/Desafio_Magalu/service/MensagemServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.Desafio_Magalu.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MensagemServiceTest {
+
+    @Test
+    void postMessage() {
+    }
+
+    @Test
+    void showById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
