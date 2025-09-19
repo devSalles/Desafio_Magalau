2e2c203182c1d69e71a52d5c46bba6234f96d14c
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-09-19 17:27:39 -0300

    Realização e conclussão da classe e dos teste unitários

--- Desafio_Magalu/src/test/java/com/example/Desafio_Magalu/service/MensagemServiceTest.java
+++ Desafio_Magalu/src/test/java/com/example/Desafio_Magalu/service/MensagemServiceTest.java
@@ -1,18 +1,142 @@
 package com.example.Desafio_Magalu.service;
 
+import com.example.Desafio_Magalu.Enum.TipoMensagem;
+import com.example.Desafio_Magalu.core.exception.IdNaoEncontrado;
+import com.example.Desafio_Magalu.dto.MensagemRequestDTO;
+import com.example.Desafio_Magalu.dto.MensagemResponseDTO;
+import com.example.Desafio_Magalu.model.Mensagem;
+import com.example.Desafio_Magalu.repository.MensagemRepository;
+import jakarta.validation.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class MensagemServiceTest {
 
+    @Mock
+    private MensagemRepository mensagemRepository;
+
+    @InjectMocks
+    private MensagemService mensagemService;
+
+    private Mensagem validMensagem;
+    private MensagemRequestDTO validMensagemDTO;
+    private static Validator validator;
+
+    @BeforeAll
+    static void setup_Validator()
+    {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator=validatorFactory.getValidator();
+    }
+
+    @BeforeEach
+    void setup()
+    {
+        validMensagemDTO = new MensagemRequestDTO();
+        validMensagemDTO.setDestinatario("Teste Mockito");
+        validMensagemDTO.setDataHoraEnvio(LocalDateTime.now());
+        validMensagemDTO.setTipoMensagem(TipoMensagem.email);
+
+        validMensagem = new Mensagem();
+        validMensagem.setId(1L);
+        validMensagem.setDestinatario(validMensagemDTO.getDestinatario());
+        validMensagem.setDataHoraEnvio(validMensagemDTO.getDataHoraEnvio());
+        validMensagem.setTipoMensagem(validMensagemDTO.getTipoMensagem());
+    }
+
+
+    //--------- TESTE METODO postMessage ---------
+
     @Test
-    void postMessage() {
+    void postMessage_DeveSalvarERetornarSucesso() {
+        when(mensagemRepository.save(any(Mensagem.class))).thenReturn(validMensagem);
+
+        Mensagem result = this.mensagemService.postMessage(validMensagemDTO);
+        assertNotNull(result);
+
+        assertEquals(validMensagemDTO.getDestinatario(),result.getDestinatario());
+        assertEquals(validMensagemDTO.getTipoMensagem(),result.getTipoMensagem());
+        assertEquals(validMensagemDTO.getDataHoraEnvio(),result.getDataHoraEnvio());
+
+        verify(mensagemRepository).save(any(Mensagem.class));
     }
 
     @Test
-    void showById() {
+    void postMessage_DeveRetornarUmaExcecao() {
+        MensagemRequestDTO mensagemRequestDTO = new MensagemRequestDTO();
+
+        mensagemRequestDTO.setDestinatario(" ");
+        mensagemRequestDTO.setDataHoraEnvio(LocalDateTime.now());
+        mensagemRequestDTO.setTipoMensagem(TipoMensagem.push);
+
+        Set<ConstraintViolation<MensagemRequestDTO>> violations = validator.validate(mensagemRequestDTO);
+
+        assertFalse(violations.isEmpty(),"As violações devem ser válidas");
+        violations.forEach(request-> System.out.println(request.getPropertyPath()+ " - " + request.getMessage()));
     }
+
+    //--------- TESTE METODO showById ---------
+
+    @Test
+    void showById_RetornarQuandoIdExistir() {
+        when(mensagemRepository.findById(anyLong())).thenReturn(Optional.of(validMensagem));
+
+        MensagemResponseDTO result = this.mensagemService.showById(1L);
+
+        assertNotNull(result);
+        assertEquals(validMensagem.getId(),result.id());
+
+        verify(mensagemRepository,times(1)).findById(1L);
+    }
+
+    @Test
+    void showById_LancarExcecaoQuandoIdNaoExistir()
+    {
+        when(mensagemRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(IdNaoEncontrado.class,()->this.mensagemService.showById(99L));
+
+        verify(mensagemRepository,times(1)).findById(99L);
+    }
+
+    //--------- TESTE METODO deleteById ---------
 
     @Test
     void deleteById() {
+        when(mensagemRepository.findById(anyLong())).thenReturn(Optional.of(validMensagem));
+        doNothing().when(mensagemRepository).delete(validMensagem);
+
+        Boolean result = this.mensagemService.deleteById(1L);
+        assertTrue(result);
+
+        verify(mensagemRepository,times(1)).findById(1L);
+        verify(mensagemRepository,times(1)).delete(validMensagem);
+    }
+
+    @Test
+    void deleteById_LancarExcecaoQuandoIdNaoExistir()
+    {
+        when(mensagemRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(IdNaoEncontrado.class,()->this.mensagemService.deleteById(1L));
+
+        verify(mensagemRepository,times(1)).findById(1L);
+        verify(mensagemRepository,never()).deleteById(1L);
     }
 }
